0288fae431e87ba51455af9198ff2f354a417f47
Starfosse <77751009+Starfosse@users.noreply.github.com>
2024-07-16 14:29:14 +0200

    filter-several-rarity

--- apps/GameClient/src/home/pages/deck/Collection.tsx
+++ apps/GameClient/src/home/pages/deck/Collection.tsx
@@ -1,9 +1,65 @@
 import usePlayerStore from "@/home/store/playerStore";
 import { useState } from "react";
-import { ActiveFilters, CardFilters, FiltersDescription } from "./cardFilters";
+import { ActiveFilters, FiltersDescription } from "./cardFilters";
 import { CardSorts, defaultSort, sorts } from "./cardSorts";
 import { DeckCardUI } from "./DeckCardUI";
 import { SortAndFilterBox } from "./SortAndFilterBox";
+import { CardType } from "@repo/ui";
+
+interface filterByRarityProps {
+  detailledCollection: (CardType & {
+    isInDeck: boolean;
+  })[];
+  currentFilter: ActiveFilters;
+}
+
+function filterByRarity({
+  detailledCollection,
+  currentFilter,
+}: filterByRarityProps) {
+  const commonFilter = currentFilter.Common
+    ? FiltersDescription["Common"].filterFunction(
+        detailledCollection,
+        currentFilter.Common
+      )
+    : null;
+  const rareFilter = currentFilter.Rare
+    ? FiltersDescription["Rare"].filterFunction(
+        detailledCollection,
+        currentFilter.Rare
+      )
+    : null;
+  const epicFilter = currentFilter.Epic
+    ? FiltersDescription["Epic"].filterFunction(
+        detailledCollection,
+        currentFilter.Epic
+      )
+    : null;
+  const legendaryFilter = currentFilter.Legendary
+    ? FiltersDescription["Legendary"].filterFunction(
+        detailledCollection,
+        currentFilter.Legendary
+      )
+    : null;
+  const tmpFilter = [commonFilter, rareFilter, epicFilter, legendaryFilter]
+    .filter((filter) => filter !== null)
+    .flatMap((filter) => filter);
+  if (tmpFilter.length !== 0) {
+    detailledCollection = detailledCollection.filter((card) =>
+      tmpFilter.includes(card)
+    );
+  }
+  detailledCollection = FiltersDescription.Level.filterFunction(
+    detailledCollection,
+    currentFilter.Level
+  );
+  detailledCollection = FiltersDescription.Cost.filterFunction(
+    detailledCollection,
+    currentFilter.Cost
+  );
+
+  return detailledCollection;
+}
 
 export default function Collection() {
   let { detailledCollection, cardNotFound } = usePlayerStore((state) => ({
@@ -14,10 +70,6 @@ export default function Collection() {
   const [isAscending, setIsAscending] = useState<boolean>(true);
 
   const [currentFilter, setCurrentFilter] = useState<ActiveFilters>({
-    Cost: {
-      min: FiltersDescription.Cost.rangeMin!,
-      max: FiltersDescription.Cost.rangeMax!,
-    },
     Common: false,
     Rare: false,
     Epic: false,
@@ -26,35 +78,25 @@ export default function Collection() {
       min: FiltersDescription.Level.rangeMin!,
       max: FiltersDescription.Level.rangeMax!,
     },
+    Cost: {
+      min: FiltersDescription.Cost.rangeMin!,
+      max: FiltersDescription.Cost.rangeMax!,
+    },
   });
 
   const collectionLength = detailledCollection.length;
-  const cardNotFoundLength = cardNotFound.length;
-  for (const filter in FiltersDescription) {
-    const typedFilter = FiltersDescription[filter as CardFilters];
-    detailledCollection = detailledCollection.filter((card) =>
-      typedFilter
-        .filterFunction(
-          detailledCollection,
-          currentFilter[filter as CardFilters]
-        )
-        .includes(card)
-    );
-  }
 
+  detailledCollection = filterByRarity({ detailledCollection, currentFilter });
   detailledCollection = sorts[currentSort].sortFunction(
     detailledCollection,
     isAscending
   );
+  cardNotFound = filterByRarity({
+    detailledCollection: cardNotFound,
+    currentFilter,
+  });
   cardNotFound = sorts[currentSort].sortFunction(cardNotFound, isAscending);
-
   detailledCollection.push(...cardNotFound);
-  // cardNotFound.forEach((card) => {
-  //   console.log(getTheLockPattern(card.id));
-  // });
-  console.log(cardNotFound);
-  // let cardNotFound;
-  // let cardNotPackable;
   return (
     <div className="flex flex-col h-[674px] w-[650px]">
       <div className="grid grid-rows-[1fr_auto] top-0 ">
