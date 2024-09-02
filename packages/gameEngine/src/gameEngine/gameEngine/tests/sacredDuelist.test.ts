6e7fdb2ab0243e38f3250d4e8c3709e2c08ebcda
Pierre LEVEQUE <pierreleveque.pro@gmail.com>
2024-09-02 13:28:35 +0200

    sacred Duelist test

---
+++ packages/gameEngine/src/gameEngine/gameEngine/tests/sacredDuelist.test.ts
@@ -0,0 +1,17 @@
+import { baseCard, initTest, sacredDuelistTest, triggerDirectAttackResolved, triggerPlaceCard } from 'game_engine';
+import { expect, test } from 'vitest';
+import { placeCardFromCardType } from '../events/normalPlaceCard';
+
+test("self card, attack longer than 45", () => {
+	const { clock, state } = initTest({ skipStartGame: true });
+	triggerPlaceCard(clock, true, 0, placeCardFromCardType({ ...baseCard, states: [sacredDuelistTest] }));
+	clock.nextTick();
+	const instanceId = state.getCard(true, 0)!.instanceId;
+	expect(state.getStateOfCard(true, 0, "sacredDuelist")).toBeDefined();
+	triggerDirectAttackResolved(clock, state, instanceId, instanceId, 10, true); // not direct attack
+	clock.nextTick();
+	expect(state.getCardByInstance(instanceId)?.hp).toBe(baseCard.hp);
+	triggerDirectAttackResolved(clock, state, instanceId, instanceId, 10); // direct attack
+	clock.nextTick();
+	expect(state.getCardByInstance(instanceId)?.hp).toBe(baseCard.hp - 10);
+});
