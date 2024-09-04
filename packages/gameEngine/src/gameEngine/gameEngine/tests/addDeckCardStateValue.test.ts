c53c7b7fb0483318f4b43356b0c4ef6acf6812af
Pierre LEVEQUE <pierreleveque.pro@gmail.com>
2024-09-04 18:29:07 +0200

    test crud deck cardState

--- packages/gameEngine/src/gameEngine/gameEngine/tests/addDeckCardStateValue.test.ts
+++ packages/gameEngine/src/gameEngine/gameEngine/tests/addDeckCardStateValue.test.ts
@@ -18,3 +18,32 @@ describe("addDeckCardStateValue", () => {
 		expect(state.getStateOfDeckCardByInstaceId(instanceId, "dummy")?.value).toBe(dummyStateTest.value! * 2);
 	});
 });
+
+test("removeDeckCardStateValue", () => {
+	const { clock, state } = initGame({ skipStartGame: true, gameData: { playerDeck: [baseCard] } });
+	const instanceId = state.playerDeck[0]!.id;
+
+	clock.triggerEvent({ type: "addDeckCardState", instanceId, state: dummyStateTest });
+	clock.nextTick();
+	expect(state.getStateOfDeckCardByInstaceId(instanceId, "dummy")).not.toBeNull();
+	clock.triggerEvent({ type: "removeDeckCardState", instanceId, stateType: dummyStateTest.type });
+	clock.nextTick();
+	expect(state.getStateOfDeckCardByInstaceId(instanceId, "dummy")).toBeNull();
+});
+
+test("increase/decrease deckCardStateValue", () => {
+	const { clock, state } = initGame({ skipStartGame: true, gameData: { playerDeck: [baseCard] } });
+	const instanceId = state.playerDeck[0]!.id;
+
+	clock.triggerEvent({ type: "addDeckCardState", instanceId, state: dummyStateTest });
+	clock.nextTick();
+	expect(state.getStateOfDeckCardByInstaceId(instanceId, "dummy")?.value).toBe(dummyStateTest.value);
+
+	clock.triggerEvent({ type: "increaseDeckCardStateValue", instanceId, stateType: dummyStateTest.type, increaseBy: 2 });
+	clock.nextTick();
+	expect(state.getStateOfDeckCardByInstaceId(instanceId, "dummy")?.value).toBe(dummyStateTest.value! + 2);
+
+	clock.triggerEvent({ type: "decreaseDeckCardStateValue", instanceId, stateType: dummyStateTest.type, decreaseBy: 1 });
+	clock.nextTick();
+	expect(state.getStateOfDeckCardByInstaceId(instanceId, "dummy")?.value).toBe(dummyStateTest.value! + 1);
+});
