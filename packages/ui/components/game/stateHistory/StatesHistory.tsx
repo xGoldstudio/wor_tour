2c2f6d8add366996e40487659d38794e45d73503
Pierre LEVEQUE <pierreleveque.pro@gmail.com>
2024-09-19 10:10:16 +0200

    auto scroll

--- packages/ui/components/game/stateHistory/StatesHistory.tsx
+++ packages/ui/components/game/stateHistory/StatesHistory.tsx
@@ -7,14 +7,21 @@ import {
 } from "@repo/ui";
 import { EffectLayout } from "../../card/Effects";
 import { AddStateEvent } from "game_engine";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ScrollContainer from "react-indiana-drag-scroll";
+
+function afterPaint(cb: () => void) {
+  requestAnimationFrame(() => {
+    requestAnimationFrame(cb);
+  });
+}
 
 export default function StatesHistory() {
   const width = 70;
   const height = 450;
 
   const [listOfStates, setListOfStates] = useState<CardState[]>([]);
+  const scrollRef = useRef<HTMLDivElement>(null);
 
   useGameEventListener<AddStateEvent>({
     type: "addState",
@@ -23,6 +30,18 @@ export default function StatesHistory() {
     },
   });
 
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      afterPaint(scrollToBottom);
+    });
+  }, [listOfStates])
+
+  function scrollToBottom() {
+    if (scrollRef.current) {
+      scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
+    }
+  }
+
   return (
     <Borders width={width} height={height} borderUnit={1} rarity={"rare"}>
       <CardIllustartion width={width} height={height} borderUnit={0.6}>
@@ -30,6 +49,7 @@ export default function StatesHistory() {
           <div className="flex w-full items-center justify-start gap-2 relative h-full">
             <ScrollContainer
               className="w-full h-full absolute flex flex-col z-10 gap-2 pt-2 items-center pb-4 overflow-y-scroll"
+              innerRef={scrollRef}
             >
               {listOfStates.map((state, index) => (
                 <EffectLayout
