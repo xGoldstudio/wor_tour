d116cbb93a53f974f2e708ee2939add56116d74e
Pierre LEVEQUE <pierreleveque.pro@gmail.com>
2024-09-20 13:31:52 +0200

    fix border bug

--- apps/GameClient/src/game/gui/PlayerGui.tsx
+++ apps/GameClient/src/game/gui/PlayerGui.tsx
@@ -63,8 +63,9 @@ function PlayerGUI({ isPlayer, clock, gameState }: PlayerGUIProps) {
                         x={8}
                         y={0}
                         width="calc(100% - 16px)"
-                        height="calc(100% - 8px)"
+                        height="100%"
                       />
+                      <rect fill="white" x={0} y="calc(100% - 8px)" width="100%" height={8} />
                     </mask>
                     <rect
                       fill="black"
